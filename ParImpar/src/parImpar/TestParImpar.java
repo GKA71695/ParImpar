2fec060263ebb5c94533741efde51988dcc0e694
Pablo <Pablo@DESKTOP-LSUDKEQ>
2023-02-02 11:14:56 +0100

    commit 2, comprobado

--- ParImpar/src/parImpar/TestParImpar.java
+++ ParImpar/src/parImpar/TestParImpar.java
@@ -4,16 +4,20 @@ import junit.framework.TestCase;
 
 public class TestParImpar extends TestCase {
 		
-	public void TestPar() {
+	public void testPar() {
 		
 		assertEquals("Par", ParImpar.parImpar(2));
+		assertEquals("Par", ParImpar.parImpar(120));
+		assertEquals("Par", ParImpar.parImpar(2006));
 	}
 	
-	public void TestImpar() {
+	public void testImpar() {
 		
-		assertEquals("Impar", ParImpar.parImpar(3));
-
+		assertEquals("Impar", ParImpar.parImpar(9));
+		assertEquals("Impar", ParImpar.parImpar(39));
+		assertEquals("Impar", ParImpar.parImpar(4011));
 	}
-		
+	
+
 }
 	
